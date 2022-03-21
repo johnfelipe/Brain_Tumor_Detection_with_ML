0b2d5cc2bdab773bf52701e89cc65d704331bbd8
Rauhan Ahmed Siddiqui <rauhaan.siddiqui@gmail.com>
2022-03-21 15:59:43 +0530

    Update app.py

--- app.py
+++ app.py
@@ -29,8 +29,9 @@ label3 = gr.outputs.Label(label="Symptoms")
 image = gr.inputs.Image()
 
 interface = gr.Interface(title = "Brain Tumor Classifier",
-             description="This an Online tool representing AI for a good cause, this online AI powered web application is built by Rauhan Ahmed Siddiqui, using this tool, one could know whether his/her brain MRI report contains a tumor or not with great accuracy, no matter how difficult it is to see that from a human eye.",
+             description="NOTE : Please crop the image using the in-built image editor on the top right section of the input window in case of too much blank space or any unecessary text such that some black blank space should be there for accurate results, approx 20-40 pixels (0.8 - 1.0 cm) each side.",
              fn=classify_image,
+             article="This an Online tool representing AI for a good cause, this online AI powered web application is built by Rauhan Ahmed Siddiqui, using this tool, one could know whether his/her brain MRI report contains a tumor or not with great accuracy, no matter how difficult it is to see that from a human eye."
              inputs=image,
              outputs=[label1, label2, label3],
              examples=[["Y3.jpg"],["21no.jpg"],["Y6.jpg"],["N17.jpg"],["Y6.jpg"]],
