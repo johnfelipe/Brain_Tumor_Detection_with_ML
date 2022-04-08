9b2fffb91a9c171d58a5d173480d60047484625b
Rauhan Ahmed Siddiqui <rauhaan.siddiqui@gmail.com>
2022-04-08 12:19:19 +0530

    Update app.py

--- app.py
+++ app.py
@@ -13,6 +13,7 @@ def classify_image(image):
   image = ImageOps.grayscale(image)
   image = np.array(image).reshape((1, -1))
   res = {labels[0]:float(model.predict_proba(image)[0][1]), labels[1]: float(model.predict_proba(image)[0][0])}
+  text = "Are you sure this is a brain MRI picture, if yes, then following are the results obtained"
   if model.predict_proba(image)[0][0] < 0.5:
       pred = "The MRI image contains a Brain Tumor"
       symptoms = "Possible Symptoms : New or increasingly strong headaches, blurred vision, loss of balance, confusion and seizures (In some cases, there may be no symptoms as well)"
@@ -20,23 +21,21 @@ def classify_image(image):
   else:
       pred = "The MRI image does not have a Brain Tumor"
       symptoms = "Possible Symptoms : None"
-  return pred, res, symptoms
+  return text, pred, res, symptoms
   
-  
-label1 = gr.outputs.Label(label="Prediction")
-label2 = gr.outputs.Label(label="Confidence Score")
-label3 = gr.outputs.Label(label="Symptoms")
+label1 = gr.outputs.Label(label="")  
+label2 = gr.outputs.Label(label="Prediction")
+label3 = gr.outputs.Label(label="Confidence Score")
+label4 = gr.outputs.Label(label="Symptoms")
 image = gr.inputs.Image()
 
 interface = gr.Interface(title = "Brain Tumor Classifier",
-             description="NOTE : Please crop the image such that its aspect ratio becomes 1:1 (square crop), leaving some empty blank space on each of the sides of image for better accuracy",
              fn=classify_image,
              article="This an Online tool representing AI for a good cause, this online AI powered web application is built by Rauhan Ahmed Siddiqui, using this tool, one could know whether his/her brain MRI report contains a tumor or not with great accuracy, no matter how difficult it is to see that from a human eye.",
              inputs=image,
-             outputs=[label1, label2, label3],
+             outputs=[label1, label2, label3, label4],
              examples=[["Y3.jpg"],["21no.jpg"],["Y6.jpg"],["N17.jpg"],["Y6.jpg"]],
              interpretation=None,
-             layout="unaligned",
              theme='dark-grass')
  
 interface.launch()
