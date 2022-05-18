41e4b0751cf41bfa864c025ada868760d572d377
felipe <ingenierofelipeurrego@gmail.com>
2022-05-18 13:55:16 -0500

    spanish

--- app.py
+++ app.py
@@ -8,34 +8,34 @@ model = pickle.load(open("catboost.pkl", "rb"))
 
 def classify_image(image):
   image = Image.fromarray(image)
-  labels = ['Brain Tumor Present', 'No Brain Tumor']
+  labels = ['Tumor Cerebral Presente', 'Sin Tumor Cerebral']
   image = image.resize((120, 120))
   image = ImageOps.grayscale(image)
   image = np.array(image).reshape((1, -1))
   res = {labels[0]:float(model.predict_proba(image)[0][1]), labels[1]: float(model.predict_proba(image)[0][0])}
-  text = "Are you sure this is a brain MRI picture? if yes, then following are the results obtained"
+  text = "¿Está seguro de que se trata de una imagen de resonancia magnética cerebral? en caso afirmativo, a continuación se obtienen los resultados"
   if model.predict_proba(image)[0][0] < 0.5:
-      pred = "The MRI image contains a Brain Tumor"
-      symptoms = "Possible Symptoms : New or increasingly strong headaches, blurred vision, loss of balance, confusion and seizures (In some cases, there may be no symptoms as well)"
+      pred = "La imagen de RMN contiene un tumor cerebral"
+      symptoms = "Posibles síntomas: dolores de cabeza nuevos o cada vez más fuertes, visión borrosa, pérdida de equilibrio, confusión y convulsiones (En algunos casos, puede que no haya síntomas también)"
   
   else:
-      pred = "The MRI image does not have a Brain Tumor"
-      symptoms = "Possible Symptoms : None"
+      pred = "La imagen de RMN no tiene un tumor cerebral"
+      symptoms = "Posibles Síntomas: Ninguno"
   return text, pred, res, symptoms
   
 label1 = gr.outputs.Label(label="")  
-label2 = gr.outputs.Label(label="Prediction")
-label3 = gr.outputs.Label(label="Confidence Score")
-label4 = gr.outputs.Label(label="Symptoms")
+label2 = gr.outputs.Label(label="Predicción")
+label3 = gr.outputs.Label(label="Puntuación de confianza")
+label4 = gr.outputs.Label(label="Síntomas")
 image = gr.inputs.Image()
 
-interface = gr.Interface(title = "Brain Tumor Classifier",
+interface = gr.Interface(title = "Clasificador de tumor cerebral",
              fn=classify_image,
-             article="This an Online tool representing AI for a good cause, this online AI powered web application is built by Rauhan Ahmed Siddiqui, using this tool, one could know whether his/her brain MRI report contains a tumor or not with great accuracy, no matter how difficult it is to see that from a human eye.",
+             article="Esta herramienta en línea que representa a la IA por una buena causa, esta aplicación web basada en IA en línea es construida por el Mgtr Jhon Felipe Urrego Mejia, usando esta herramienta, uno podría saber si su informe de resonancia magnética cerebral contiene un tumor o no con gran precisión, no importa lo difícil que es ver eso de un ojo humano.",
              inputs=image,
              outputs=[label1, label2, label3, label4],
              examples=[["images/Y3.jpg"],["images/21no.jpg"],["images/Y6.jpg"],["images/N17.jpg"],["images/Y6.jpg"]],
              interpretation=None,
              theme='dark-grass')
  
-interface.launch()
+interface.launch(server_name="0.0.0.0")
